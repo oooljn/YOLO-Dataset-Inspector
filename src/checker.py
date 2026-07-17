ccc68fc373e5ca38e8f5135e8e20bfd27b99d060
oooljn <18810055229@163.com>
2026-07-17 14:10:09 +0800

    Add dataset checker module

---
+++ src/checker.py
@@ -0,0 +1,12 @@
+"""
+Dataset checker module.
+
+Check YOLO format dataset:
+- image-label matching
+- label format
+- bbox validity
+"""
+
+
+def check_dataset():
+    print("Dataset checking module")
